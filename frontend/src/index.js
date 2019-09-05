987a17bb7c2e729f466e97b0bce48da9a83b27e3
apdlb <alepdlb@gmail.com>
2019-09-05 10:34:34 +0200

    Add redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,24 @@
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { persistor, store } from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootComponent = (
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
+ReactDOM.render(rootComponent, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
